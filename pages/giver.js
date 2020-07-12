369ecd8413c967c1757095949e81335e7676821f
Rodrigo Alcala <alcala@myego2go.de>
2020-07-12 22:21:01 +0200

    chore: move graphQL query (addShoe) from ShoeForm to its page (giver)

--- pages/giver.js
+++ pages/giver.js
@@ -1,6 +1,26 @@
+import { useMutation } from '@apollo/react-hooks';
+import gql from 'graphql-tag';
+
 import { withApollo } from './../lib/apollo';
+
 import ShoeForm from './../components/ShoeForm';
 
-const Giver = () => <ShoeForm />;
+const ADD_SHOE = gql`
+  mutation addShoe($shoe: ShoeInput) {
+    addShoe(shoe: $shoe) {
+      success
+      message
+      shoe {
+        _id
+      }
+    }
+  }
+`;
+
+const Giver = () => {
+  const [addShoe] = useMutation(ADD_SHOE);
+
+  return <ShoeForm addShoe={addShoe} />;
+};
 
 export default withApollo(Giver);
