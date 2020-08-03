d1f0264f50c38337594f9587bf2faa0ad1298591
Rodrigo Alcala <rodri.a90@gmail.com>
2020-08-03 22:01:43 +0200

    style(Shoe): first paint of the ShoeBox

--- components/Shoe.js
+++ components/Shoe.js
@@ -1,3 +1,25 @@
-const Shoe = ({ shoe }) => <p>ID: {shoe._id}</p>;
+import styled from 'styled-components';
+
+const ShoeBox = styled.div`
+  background-image: url(${(props) => props.backgroundImage || '/silhouette.png'});
+  background-repeat: no-repeat;
+  background-size: 100%;
+  padding-top: 100%;
+  position: relative;
+  text-align: center;
+
+  > p {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+`;
+
+const Shoe = ({ shoe }) => (
+  <ShoeBox>
+    <p>ID: {shoe._id}</p>
+  </ShoeBox>
+);
 
 export default Shoe;
