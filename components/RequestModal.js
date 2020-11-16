54d5e1771cd3c670f0f7bb853c8ebfaebf1f529b
Rodrigo Alcala <rodri.a90@gmail.com>
2020-11-16 20:10:48 +0100

    style(RequestPortal): make modal take up the whole screen

--- components/RequestModal.js
+++ components/RequestModal.js
@@ -1,15 +1,26 @@
 import usePortal from '../lib/usePortal';
+import styled from 'styled-components';
+
+const ModalContainer = styled.div`
+  background-color: rgba(0, 0, 0, 0.5);
+  position: fixed;
+  height: 100%;
+  width: 100%;
+  top: 0;
+  left: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
 
 const RequestModal = () => {
   const ModalPortal = usePortal();
 
   return (
     <ModalPortal>
-      <div className="overlay">
-        <div className="content">
-          <h2>This header is inside modal</h2>
-        </div>
-      </div>
+      <ModalContainer>
+        <h2>TUVIEJA</h2>
+      </ModalContainer>
     </ModalPortal>
   );
 };
